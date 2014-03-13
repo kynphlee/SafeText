3989221f30be0708282d73ecba52ef67947e7327
kynphlee <kynphlee@NeXuS-Main>
2014-03-13 01:53:52 -0400

    Added constants.

--- src/com/modernmotion/safetext/util/STConstants.java
+++ src/com/modernmotion/safetext/util/STConstants.java
@@ -1,8 +1,12 @@
-package com.modernmotion.safetext;
+package com.modernmotion.safetext.util;
 
 public class STConstants {
 	public static final String DEBUG_STRING = "ST:";
 	public static final String DEBUG_ACCELEROMETER = DEBUG_STRING + "ACCEL";
 	public static final String DEBUG_SENSOR_MANAGER = DEBUG_STRING + "SENSORMANAGER";
 	public static final String DEBUG_SENSOR = DEBUG_STRING + "SENSOR";
+	public static final double ST_THRESHOLD = 2.2352; //5 mph
+	public static final float SCAN_WINDOW = 10.0f;
+	public static final double MS2S = 0.001;
+	public static final float NS2S = 1.0f / 1000000000.0f;
 }
